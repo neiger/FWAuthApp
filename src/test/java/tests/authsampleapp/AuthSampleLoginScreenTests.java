7ed48ad69bfb2ddcfc9a9dbe671d3c4b1b42a62c
neiger <neiger.serrano.wks@gmail.com>
2023-06-09 10:06:34 -0600

    revokeToken implementation

--- src/test/java/tests/authsampleapp/AuthSampleLoginScreenTests.java
+++ src/test/java/tests/authsampleapp/AuthSampleLoginScreenTests.java
@@ -62,8 +62,21 @@ public class AuthSampleLoginScreenTests extends MobileDriverManager {
             assertTrue(authSampleLoginScreen.checkAlerMsgPrint(), basicErrorMsg(""));
         }
         assertAll();
+    }
 
-
+    @Test  @Parameters({"deviceType"})
+    public void revokeTokenTest(String deviceType) {
+        if(deviceType.equals("nonGMS")) {
+            WebViewBrowserScreen webViewBrowserScreen = authSampleLoginScreen.signInFromBrowser();
+            assertTrue(webViewBrowserScreen.verifySignPageLoads(), basicErrorMsg("The signIn web view was not loaded correctly"));
+            assertTrue(webViewBrowserScreen.clickLoggedInAccountXY(540,700), basicErrorMsg("Unable to click on the XY location given"));
+            authSampleLoginScreen = webViewBrowserScreen.returnAsSignInState(800,2025);
+        } else {
+            assertTrue(authSampleLoginScreen.verifySignInPopUpShown(), basicErrorMsg("Unable to shown the pop up account"));
+        }
+        assertTrue(authSampleLoginScreen.verifySignInState(), basicErrorMsg("The signed in state fails the validation"));
+        assertTrue(authSampleLoginScreen.tapRevokeToken(), basicErrorMsg("Unable to tap on the RevokeToken button"));
+        assertAll();
     }
 
 }
